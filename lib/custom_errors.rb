ccc292dfde3c9f875d42035fe62213b292f6ddaf
Parker Williamson <parker.williamson@gmail.com>
2020-05-21 18:25:44 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,9 +8,18 @@ class Person
   def get_married(person)
     self.partner = person
     person.partner = self
+    if person.class != Person 
+      raise PartnerError 
+    else
+      person.partner = self 
+    end
   end
-
+  
+  class Partner Error < StandardError 
+    
+  end
 end
+
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
